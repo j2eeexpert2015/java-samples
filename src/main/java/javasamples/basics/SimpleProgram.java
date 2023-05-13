171bbd7955cd4b0d2e68bee1d7f37b55a5f4323d
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-05-13 12:36:47 +0530

    Initial commit

---
+++ src/main/java/javasamples/basics/SimpleProgram.java
@@ -0,0 +1,15 @@
+package javasamples.basics;
+
+/*
+This is a simple Java program. (File name:
+SimpleProgram.java)
+*/
+class SimpleProgram {
+    public static void main(String args[]) {
+        printHelloWorld();
+    }
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
+
